dd0ad1336469be7ecf418cdaba933135ae62a2a6
marif52 <mariferen52@gmail.com>
2025-12-08 16:59:42 +0100

    "First Main Page Template with close, and change Full Screen"

--- src/main/java/com/isep/smarttripplanner/MainApplication.java
+++ src/main/java/com/isep/smarttripplanner/MainApplication.java
@@ -1,8 +1,11 @@
 package com.isep.smarttripplanner;
 
+import com.sun.tools.javac.Main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.Screen;
 import javafx.stage.StageStyle;
@@ -13,17 +16,19 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Parent root = fxmlLoader.load();
+        MainController controller = fxmlLoader.getController();
+        controller.setStage(stage);
         double screenWidth = Screen.getPrimary().getVisualBounds().getWidth() * 0.7;
         double screenHeight = screenWidth * (9.0/16.0);
-
-        Scene scene = new Scene(fxmlLoader.load(), screenWidth, screenHeight);
+        Scene scene = new Scene(root, screenWidth, screenHeight);
         stage.initStyle(StageStyle.TRANSPARENT);
-        scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
         stage.setTitle("Main Page");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.centerOnScreen();
-        stage.setFullScreen(false);
+        stage.setFullScreen(true);
         stage.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.isep.smarttripplanner {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires jdk.compiler;
 
 
     opens com.isep.smarttripplanner to javafx.fxml;
